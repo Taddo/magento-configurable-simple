be7c1739440e9996d83e3559ba8a96b9649db81e
Taddo <taddo.350z@gmail.com>
2016-09-08 10:54:34 +0200

    Added Info to select

--- app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Model/Product/Type/Simple.php
+++ app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Model/Product/Type/Simple.php
@@ -2,29 +2,9 @@
 class OrganicInternet_SimpleConfigurableProducts_Catalog_Model_Product_Type_Simple
     extends Mage_Catalog_Model_Product_Type_Simple
 {
-    #Later this should be refactored to live elsewhere probably,
-    #but it's ok here for the time being
-    private function getCpid()
-    {
-        $cpid = $this->getProduct()->getCustomOption('cpid');
-        if ($cpid) {
-            return $cpid;
-        }
-
-        $br = $this->getProduct()->getCustomOption('info_buyRequest');
-        if ($br) {
-            $brData = unserialize($br->getValue());
-            if(!empty($brData['cpid'])) {
-                return $brData['cpid'];
-            }
-        }
-
-        return false;
-    }
-
     public function prepareForCart(Varien_Object $buyRequest, $product = null)
     {
-        $product = $this->getProduct($product);
+        $product = $this->getProduct($product)->addAttributeToSelect('info');
         parent::prepareForCart($buyRequest, $product);
         if ($buyRequest->getcpid()) {
             $product->addCustomOption('cpid', $buyRequest->getcpid());
@@ -34,13 +14,9 @@ class OrganicInternet_SimpleConfigurableProducts_Catalog_Model_Product_Type_Simp
 
     public function hasConfigurableProductParentId()
     {
-        $cpid = $this->getCpid();
-        Mage::log("cpid: ". $cpid);
-        return !empty($cpid);
-    }
-
-    public function getConfigurableProductParentId()
-    {
-        return $this->getCpid();
+        if ($this->getProduct()->getCustomOption('cpid')) {
+            return true;
+        }
+        return false;
     }
 }
